12d5e3262107e81d96523cfe865051c4ae01b19d
roham-tgh <itsgalaxya55@gmail.com>
2025-04-05 18:27:52 +0330

    adds some classes

---
+++ src/models/DateTime.java
@@ -0,0 +1,18 @@
+package models;
+
+public class DateTime {
+    int hour;
+    int minutes;
+    int day;
+    int month;
+    int season;
+
+    public void startNewDay(){}
+
+
+    public String getTime(){}
+    public String getDate(){}
+    public String getDateTime{}
+    public String getDayOfTheWeek(){}
+
+}
---
+++ src/models/enums/CreateTool.java
@@ -0,0 +1,8 @@
+package models.enums;
+
+import models.Tool;
+
+public enum CreateTool {
+    PICKAXE(new Tool()),
+    HOE(new Tool())
+}
