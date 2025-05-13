e8a9f6d24497c59620b5553b1f1a3b48d6fa6a27
hassanp30 <hassanpanahi30@gmail.com>
2025-05-13 11:52:36 +0330

    recipe changes

--- src/models/Crafting/Recipe.java
+++ src/models/Crafting/Recipe.java
@@ -1,11 +1,20 @@
 package models.Crafting;
 
 import models.ItemFaces.InventoryItem;
+import models.ItemFaces.Item;
 
-import java.util.Map;
+import java.util.ArrayList;
 
 public class Recipe {
-    private Map<InventoryItem, Integer> ingredientAmounts;
+    private ArrayList<InventoryItem> ingredients;
+    private Item craftedItem;
+    int time; // in hours
+
+    public Recipe(ArrayList<InventoryItem> ingredients, Item craftedItem, int time) {
+        this.ingredients = ingredients;
+        this.craftedItem = craftedItem;
+        this.time = time;
+    }
 
     public boolean canCraft() {
         // Implement logic to check if the recipe can be crafted
